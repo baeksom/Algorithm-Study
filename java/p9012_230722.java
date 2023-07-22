c4a5483a0c3b01490000166279daa679a590402e
Somi <backsomi@naver.com>
2023-07-22 11:43:16 +0900

    0722

---
+++ java/p9012_230722.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p9012_230722 {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for(int i=0; i<n; i++){
+            
+        }
+    }
+}
