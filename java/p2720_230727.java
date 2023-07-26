3f52bc32a813e20a618b1ff4e9467bea38d2e513
Somi <backsomi@naver.com>
2023-07-27 01:31:07 +0900

    세탁소 사장 동혁_230727

---
+++ java/p2720_230727.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p2720_230727 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+        int n = Integer.parseInt(br.readLine());
+        while(n-- >0){
+            int coin = Integer.parseInt(br.readLine());
+            sb.append(coin/25).append(" ");
+            coin %= 25;
+            sb.append(coin/10).append(" ");
+            coin %= 10;
+            sb.append(coin/5).append(" ");
+            coin %= 5;
+            sb.append(coin).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
