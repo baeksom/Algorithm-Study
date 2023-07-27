e1ea5b25f30220ec18c1dbaa621c250ed8365c5b
baeksom <97323941+baeksom@users.noreply.github.com>
2023-07-27 23:19:43 +0900

    N-Queen_230727

--- java/p9663_230727.java
+++ java/p9663_230727.java
@@ -19,15 +19,23 @@ public class p9663_230727 {
             return;
         }
         for(int i=0; i<n; i++){
-            if(num == 0){
-                arr[num] = i;
+            arr[num] = i;
+            if(check(num)){
                 nQueen(num+1);
-            } else{
-                if(i != arr[num-1] && Math.abs(i-arr[num-1]) != 1){
-                    arr[num] = i;
-                    nQueen(num+1);
-                }
             }
         }
+    }
+    public static boolean check(int col){
+        for(int i=0; i<col; i++){
+            if(arr[i] == arr[col]){
+                // 행이 같을 때
+                return false;
+            }
+            else if(Math.abs(arr[i] - arr[col]) == Math.abs(i - col)){
+                // 대각선이 겹칠 때 (행의 차와 열의 차가 같을 때)
+                return false;
+            }
+        }
+        return true;
     }
 }
