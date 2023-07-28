d9cc285b6fac99c98c85c3909c3ed6795d27167b
baeksom <97323941+baeksom@users.noreply.github.com>
2023-07-28 15:06:09 +0900

    소수 구하기_230728

---
+++ java/p1929_230728.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p1929_230728{
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        
+    }
+}
