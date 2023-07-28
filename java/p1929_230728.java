82b5a2d13ba2cb181cee7959a5e64a946e89e330
baeksom <97323941+baeksom@users.noreply.github.com>
2023-07-28 22:21:29 +0900

    소수 구하기_230728

--- java/p1929_230728.java
+++ java/p1929_230728.java
@@ -1,14 +1,27 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class p1929_230728{
     public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
-
-        
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        for(int i = m; i <= n; i++){
+            if(check(i)) sb.append(i).append('\n');
+        }
+        System.out.println(sb);
+    }
+    public static boolean check(int num){
+        if(num < 2){
+            return false;
+        }
+        else{
+            for(int i=2; i*i<=num; i++){
+                if(num % i == 0){
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
