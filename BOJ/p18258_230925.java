eff5ddaec6cf6144df99c59eb9bf3f76b75a7d00
baeksom <97323941+baeksom@users.noreply.github.com>
2023-09-25 18:47:57 +0900

    카드2_230925

--- BOJ/p18258_230925.java
+++ BOJ/p18258_230925.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
+public class p18258_230925 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
